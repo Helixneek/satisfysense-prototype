e45509d47d13d8591f1c9851f4bea84b0db8faf3
Nico Marcelino <88550178+Helixneek@users.noreply.github.com>
2023-01-18 17:52:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function showFiles() {
+    // An empty img element
+    let demoImage = document.getElementById('idImage');
+    // read the file from the user
+    let file = document.querySelector('input[type=file]').files[0];
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        demoImage.src = reader.result;
+    }
+    reader.readAsDataURL(file);
+    app();
+}  
+
+
+
+async function load() {
+    const model = await tf.loadLayersModel('tfjs_model/model.json');
+    return model;
+  };
+
+const model = load();  // load the model now to prevent any delay when user clicks 'Predict'
+
+async function predict(){
+    img_ = document.getElementById('idImage');
+    if (img_.src != ""){
+        const result = await net.classify(img_);
+        console.log(result);
+    }
+}
